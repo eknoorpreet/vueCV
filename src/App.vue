a446ae647238e754c68866911f2975adaa1a9e49
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-07-08 03:54:21 -0700

    Added dummy resume content

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,78 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
-</script>
-
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
-      Welcome
+  <main class="container">
+    <div id="resume" class="d-flex">
+      <div class="left-col">
+        <div class="resume-section">
+          <h4 class="section-headline">About Me</h4>
+          <div>
+            From data cleaning to data analysis to machine learning, I am
+            passionate about everything data
+          </div>
+        </div>
+        <div class="resume-section">
+          <h4 class="section-headline">Contact</h4>
+          <ul>
+            <li>0123456789</li>
+            <li>contact@gmail.com</li>
+            <li>Quebec St, V5N 2C7, Vancouver, BC, Canada</li>
+          </ul>
+        </div>
+        <div class="resume-section">
+          <h4 class="section-headline">Skills</h4>
+          <ul>
+            <li>Vue</li>
+            <li>Java</li>
+            <li>PHP</li>
+            <li>Python</li>
+            <li>TailwindCSS</li>
+          </ul>
+        </div>
+        <div class="resume-section">
+          <h4 class="section-headline">Certifications</h4>
+          <ul>
+            <li>AWS Cloud Practioner (Amazon Web Services)</li>
+          </ul>
+        </div>
+      </div>
+      <div class="right-col">
+        <div class="personal-name">Eknoorpreet Singh</div>
+        <div class="personal-title">Software Engineer (Front-end)</div>
+        <div class="resume-section">
+          <h4 class="section-headline">Experience</h4>
+          <ul>
+            <li>
+              Developed responsive and interactive web applications using modern
+              front-end frameworks such as Vue.js, React, and Angular, improving
+              user engagement by 25%.
+            </li>
+            <li>
+              Collaborated with UX/UI designers to implement user-friendly
+              interfaces, ensuring cross-browser compatibility and maintaining
+              performance optimization across all devices.
+            </li>
+            <li>
+              Integrated RESTful APIs and third-party services to enhance
+              functionality and streamline data flow, resulting in a 30%
+              reduction in page load times.
+            </li>
+            <li>
+              Utilized version control systems like Git for efficient project
+              management and collaborated with back-end developers to ensure
+              seamless integration and deployment of web applications.
+            </li>
+          </ul>
+        </div>
+        <div class="resume-section">
+          <h4 class="section-headline">Education</h4>
+          <div>University of British Columbia, Vancouver, BC, 2015 - 2019</div>
+          <ul>
+            <li>Relevant coursework in algorithms and programming languages</li>
+            <li>Project: Recommendation system for Amazon</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
