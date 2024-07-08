6195acac3d5c990798f125919cd48ff5f2801216
Eknoorpreet Singh <eknoorpreet2@gmail.com>
2024-07-08 03:55:12 -0700

    added basic styling

--- src/App.vue
+++ src/App.vue
@@ -75,4 +75,74 @@
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+#resume {
+  box-shadow: rgba(50, 50, 93, 0.25) 0 13px 27px -5px,
+    rgba(0, 0, 0, 0.3) 0 8px 16px -8px;
+  height: 297mm;
+  width: 210mm;
+}
+
+.left-col {
+  background-color: var(--background-color-left);
+  color: var(--text-color-left);
+  border-right: 1px solid var(--highlight-color-left);
+  width: 30%;
+  padding: 30px;
+}
+
+.right-col {
+  background-color: var(--background-color-right);
+  color: var(--text-color-right);
+  width: 70%;
+  padding: 30px;
+}
+
+.section-headline {
+  font-size: 20px;
+  font-weight: var(--headline-weight);
+  margin-bottom: 15px;
+  margin-top: 0;
+}
+
+.left-col .section-headline {
+  border-bottom: 1px solid var(--highlight-color-left);
+  padding-bottom: 5px;
+  margin-right: -30px;
+  padding-right: 10px;
+  color: var(--highlight-color-left);
+}
+
+.right-col .section-headline {
+  color: var(--highlight-color-right);
+}
+
+.resume-section {
+  margin-bottom: 30px;
+}
+
+.personal-name {
+  font-weight: 300;
+  color: var(--highlight-color-right);
+  font-size: 28px;
+  border-bottom: 1px solid var(--highlight-color-right);
+  margin: 0;
+  margin-left: -30px;
+  padding-left: 30px;
+  padding-bottom: 15px;
+}
+
+.personal-title {
+  border-bottom: 1px solid var(--highlight-color-right);
+  margin: 0 0 20px -30px;
+  padding: 15px 0 15px 30px;
+  font-weight: 300;
+  font-size: 20px;
+}
+
+#resume ul {
+  padding-inline-start: 16px;
+  margin-block-end: 0px;
+  margin-block-start: 5px;
+}
+</style>
